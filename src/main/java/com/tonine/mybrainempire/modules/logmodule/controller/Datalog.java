12efe02e50d90d72690b00697d29cd89d793bc70
xiongyige <xiongyige@aspirecn.com>
2022-01-04 14:22:32 +0800

    marker过滤消息测试。

--- src/main/java/com/tonine/mybrainempire/modules/logmodule/controller/Datalog.java
+++ src/main/java/com/tonine/mybrainempire/modules/logmodule/controller/Datalog.java
@@ -2,6 +2,9 @@ package com.tonine.mybrainempire.modules.logmodule.controller;
 
 import com.tonine.mybrainempire.common.entity.Result;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +22,12 @@ public class Datalog {
     public Result<String> datalog(String type,String data){
 
         log.info("test");
+        Marker pageExposure1 = MarkerManager.getMarker("pageExposure");
+        String dataString;
+        for (int i = 0; i < 10000; i++) {
+            dataString= RandomStringUtils.randomAlphanumeric(100)+type+"你好";
+            log.info(pageExposure1,dataString);
+        }
         Result<String> objectResult = new Result<>();
         objectResult.success("success");
         return objectResult;
