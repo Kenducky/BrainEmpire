0d8bbe929905a4686bf033277be4ea711e861b0e
xiongyige <xiongyige@aspirecn.com>
2022-01-18 15:40:43 +0800

    添加test

--- MyBrainEmpire-SpringBoot/src/test/java/com/tonine/mybrainempire/MyBrainEmpireApplicationTests.java
+++ MyBrainEmpire-SpringBoot/src/test/java/com/tonine/mybrainempire/MyBrainEmpireApplicationTests.java
@@ -1,13 +1,44 @@
 package com.tonine.mybrainempire;
 
+import com.tonine.mybrainempire.modules.testmodule.dao.BlogsMapper;
+import com.tonine.mybrainempire.modules.testmodule.entity.Blogs;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class MyBrainEmpireApplicationTests {
+    @Autowired
+    BlogsMapper blogsMapper;
 
     @Test
-    void contextLoads() {
+    void test() {
+        System.out.println("testStart");
+        List<Blogs> blogs = blogsMapper.selectAllByTitleLike("test");
+        List<Blogs> blogs1 = new ArrayList<>();
+        Assertions.assertNotNull(blogs,"空了！怎么回事呢");
+        Assertions.assertEquals(0, blogsMapper.selectAllByTitleLike("k").size(),"怎么回事，有数据");
+
+        List<String> testss= new ArrayList<>();
+        testss.add("");
+        testss.add("123123");
+        testss.add(null);
+        testss.add("4123123");
+
+        List<String> msisdns = testss.stream()
+                .filter(Objects::nonNull)
+                .sorted()
+                .collect(Collectors.toList());
+
+
+
+        System.out.println("testStop!");
 
     }
 
