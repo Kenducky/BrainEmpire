257925cfd6aeba21d1cbe89a760209073580f1fc
xiongyige <xiongyige@aspirecn.com>
2022-02-08 09:43:46 +0800

    修改about，修改test

--- MyBrainEmpire-SpringBoot/src/test/java/com/tonine/mybrainempire/MyBrainEmpireApplicationTests.java
+++ MyBrainEmpire-SpringBoot/src/test/java/com/tonine/mybrainempire/MyBrainEmpireApplicationTests.java
@@ -1,46 +1,34 @@
 package com.tonine.mybrainempire;
 
 import com.tonine.mybrainempire.modules.testmodule.dao.BlogsMapper;
-import com.tonine.mybrainempire.modules.testmodule.entity.Blogs;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @SpringBootTest
 class MyBrainEmpireApplicationTests {
     @Autowired
     BlogsMapper blogsMapper;
 
+    static final int MAXIMUM_CAPACITY = 1 << 30;
     @Test
     void test() {
         System.out.println("testStart");
-        List<Blogs> blogs = blogsMapper.selectAllByTitleLike("test");
-        List<Blogs> blogs1 = new ArrayList<>();
-        Assertions.assertNotNull(blogs,"空了！怎么回事呢");
-        Assertions.assertEquals(0, blogsMapper.selectAllByTitleLike("k").size(),"怎么回事，有数据");
 
-        List<String> testss= new ArrayList<>();
-        testss.add("");
-        testss.add("123123");
-        testss.add(null);
-        testss.add("4123123");
-
-        List<String> msisdns = testss.stream()
-                .filter(Objects::nonNull)
-                .sorted()
-                .collect(Collectors.toList());
-
-
-
+        System.out.println("tableSizeFor(10) = " + tableSizeFor(10));
         System.out.println("testStop!");
-
     }
+
+    int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
 
 
 }
--- mybrainempire-vue/src/views/About.vue
+++ mybrainempire-vue/src/views/About.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <h1>再来一个H3</h1>
   </div>
 </template>
+<script>
+const x = 10;
+console.log("x="+x);
+</script>
